17cb105c57312b0c9a65d10da6fc6243e135f6ad
Rafael de Jesus <rafaelbr33@hotmail.com>
2023-10-16 14:56:25 -0300

    add docker comment to client_predict

--- homework05/client_predict.py
+++ homework05/client_predict.py
@@ -1,5 +1,11 @@
 import requests
 
+# Local Flask server
 url = "http://127.0.0.1:5000/predict"
 client = {"job": "unknown", "duration": 270, "poutcome": "failure"}
+
+# To Run the docker version
+# url = "http://127.0.0.1:9696/predict"
+# client = {"job": "retired", "duration": 445, "poutcome": "success"}
+
 print(requests.post(url, json=client).json())
